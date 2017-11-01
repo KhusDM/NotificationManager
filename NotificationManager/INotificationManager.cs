8f3328f12110882bec6f46c6b0b9a3b3a3d3cdae
Khusnetdinov Dmitrii <d.xusnetd@yandex.ru>
2017-11-02 00:27:13 +0300

    third commit

--- NotificationManager/INotificationManager.cs
+++ NotificationManager/INotificationManager.cs
@@ -4,16 +4,22 @@ namespace NotificationManager
 {
      public interface INotificationManager
     {
+        //Обработать сигнал
         void ProcessSignal(string signal);
 
+        //Добавить адресата для сигнала
         void AddRecipientForSignal(string email, string signal);
 
+        //Исключить адресата из сигнала
         void RemoveRecipientForSignal(string email, string signal);
 
+        //Получить список поддерживаемых сигналов
         IEnumerable<string> ListOfSupportedSignals();
 
+        //Добавить сигнал
         void AddSignal(string signal);
 
+        //Проверить будет ли адресат получать уведомление для сигнала
         bool CheckNotificationSignal(string email, string signal);
     }
 }
